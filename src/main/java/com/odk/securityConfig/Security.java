d1f91295bd7d939717c21f9c8ec5034a08d867e9
Abdramane Cissé <cisseodl@gmail.com>
2026-03-12 11:00:48 +0100

    Persistance des images sur S3

--- src/main/java/com/odk/securityConfig/Security.java
+++ src/main/java/com/odk/securityConfig/Security.java
@@ -3,6 +3,7 @@ package com.odk.securityConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
